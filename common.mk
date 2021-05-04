d38687eebc233748c6cf0e8b3a6712d627468d3f
P.Adarsh Reddy <padarshr@codeaurora.org>
2021-05-04 15:29:23 +0530

    sm8550-common: Enforce android-S new launch debugfs restrictions
    Change-Id: I78878e1c4177c07d66b13e5659d4360252c5f87b

--- common.mk
+++ common.mk
@@ -66,6 +66,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# DebugFS
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
