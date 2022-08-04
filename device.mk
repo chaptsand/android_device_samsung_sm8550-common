f7dd9a60936452774bbc3f6ab301c48659ff46f5
Arian <arian.kulmer@web.de>
2022-08-04 23:16:06 +0200

    dm2q: Enable 64bit audio HAL
    Change-Id: If4734e561ff5f32aabbc5c8a31cbc40813c50650

--- device.mk
+++ device.mk
@@ -34,10 +34,15 @@ PRODUCT_PACKAGES += \
     otapreopt_script
 
 # Audio
+SOONG_CONFIG_NAMESPACES += android_hardware_audio
+SOONG_CONFIG_android_hardware_audio += \
+    run_64bit
+SOONG_CONFIG_android_hardware_audio_run_64bit := true
+
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio.service_64 \
+    android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
