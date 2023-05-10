eaaf659b85c9f8e394bdd6edc3ba85c14de9db28
Sai Priyanka Thirukkovela <quic_thirukko@quicinc.com>
2023-05-10 12:27:51 +0530

    sm8550-common: Add head_tracker to permissions for spatial audio
    copy android.hardware.sensor.dynamic.head_tracker.xml to
    permissions for dynamic spatial audio
    
    Change-Id: I69a3802deadba3479f8c2a7c07fdeef73e9cb6b8

--- common.mk
+++ common.mk
@@ -62,6 +62,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
+    frameworks/native/data/etc/android.hardware.sensor.dynamic.head_tracker.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.dynamic.head_tracker.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 # Bluetooth
