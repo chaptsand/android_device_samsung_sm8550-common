91817eb7073840cf031de5216769e47514e26d41
chaptsand <chaptsand@gmail.com>
2023-09-26 16:22:44 +0800

    dm2q: Create WiFi firmware symlinks
    Change-Id: Ideddbc10347afe40e0e57621cbb8e2a5ab580b1e

--- Android.mk
+++ Android.mk
@@ -102,11 +102,20 @@ $(RFS_MSM_WPSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf /vendor/firmware_mnt $@/readonly/firmware
 	$(hide) ln -sf /vendor/firmware $@/readonly/vendor/firmware
 
+WIFI_FIRMWARE_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/
+$(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating WiFi firmware symlinks: $@"
+	@mkdir -p $@/wlan/qca_cld/qca6490
+	$(hide) ln -sf /data/vendor/firmware/wlanmdsp.mbn $@/wlanmdsp.otaupdate
+	$(hide) ln -sf /mnt/vendor/persist/qca6490/wlan_mac.bin $@/wlan/qca_cld/qca6490/wlan_mac.bin
+	$(hide) ln -sf /vendor/etc/wifi/qca6490/WCNSS_qcom_cfg.ini $@/wlan/qca_cld/qca6490/WCNSS_qcom_cfg.ini
+
 ALL_DEFAULT_INSTALLED_MODULES += \
     $(RFS_MSM_ADSP_SYMLINKS) \
     $(RFS_MSM_CDSP_SYMLINKS) \
     $(RFS_MSM_MPSS_SYMLINKS) \
     $(RFS_MSM_SLPI_SYMLINKS) \
-    $(RFS_MSM_WPSS_SYMLINKS)
+    $(RFS_MSM_WPSS_SYMLINKS) \
+    $(WIFI_FIRMWARE_SYMLINKS)
 
 endif
