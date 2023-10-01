a95b937c540375eb63198f025274743d372204b7
chaptsand <chaptsand@gmail.com>
2023-10-01 23:21:47 +0800

    sm8550-common: Import device framework matrix
    * Required by FCM Version 7
    
    Change-Id: Ic0c44a805836c6889645d2d91f1d36058bf1d9bc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -81,6 +81,10 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/vintf/device_framework_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/vintf/manifest_kalama.xml \
     $(COMMON_PATH)/vintf/manifest_samsung.xml
