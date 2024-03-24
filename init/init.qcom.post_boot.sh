14f9a377259a1a0704349fbf66b443b162205a7f
chaptsand <chaptsand@gmail.com>
2024-03-24 16:34:51 +0800

    sm8550-common: Configure RAM Plus
    Change-Id: I19387c70498ca93f155329fdaf4c4f27a167f88f

--- init/init.qcom.post_boot.sh
+++ init/init.qcom.post_boot.sh
@@ -709,10 +709,10 @@ low_ram=`getprop ro.config.low_ram`
 
 if [ "$ProductName" == "msmnile" ] || [ "$ProductName" == "kona" ] || [ "$ProductName" == "sdmshrike_au" ]; then
       # Enable ZRAM
-      configure_zram_parameters
+      # configure_zram_parameters
       configure_read_ahead_kb_values
       echo 0 > /proc/sys/vm/page-cluster
-      echo 100 > /proc/sys/vm/swappiness
+      # echo 100 > /proc/sys/vm/swappiness
 else
     arch_type=`uname -m`
 
@@ -817,13 +817,13 @@ else
     # Set allocstall_threshold to 0 for all targets.
     # Set swappiness to 100 for all targets
     echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-    echo 100 > /proc/sys/vm/swappiness
+    # echo 100 > /proc/sys/vm/swappiness
 
     # Disable wsf for all targets beacause we are using efk.
     # wsf Range : 1..1000 So set to bare minimum value 1.
     echo 1 > /proc/sys/vm/watermark_scale_factor
 
-    configure_zram_parameters
+    # configure_zram_parameters
 
     configure_read_ahead_kb_values
 
@@ -4293,7 +4293,7 @@ case "$target" in
 
             # Turn on sleep modes.
             echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-            echo 100 > /proc/sys/vm/swappiness
+            # echo 100 > /proc/sys/vm/swappiness
             ;;
         esac
     ;;
@@ -4849,7 +4849,7 @@ case "$target" in
 	echo N > /sys/module/lpm_levels/L3/l3-dyn-ret/idle_enabled
         # Turn on sleep modes.
         echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-	echo 100 > /proc/sys/vm/swappiness
+	# echo 100 > /proc/sys/vm/swappiness
 	echo 120 > /proc/sys/vm/watermark_scale_factor
     ;;
 esac
