68366ae24153189659636a3561e461c49b3ab3fd
Simon1511 <me@simon1511.de>
2024-03-27 19:39:45 +0800

    sm8550-common: libsec-ril: Always return an empty value for GET_SMSC_ADDRESS
    * Make parameter of type RilData (second last one) passed to SecRil::RequestComplete()
      in OnGetSmscAddressDone() be NULL, so that the framework passes an empty value when sending SMS
    
    * Before U this value was not being used, on U the value returned is "SMSC",format_specifier
      which is considered invalid when passed back to RIL for sending SMS, however when an empty value
      is passed RIL will select the default SMSC, like it did on T and sending SMS works again
    
    * For the SMSC patch, SecRil::RequestComplete() in OnGetSmscAddressDone()
      now looks like this instead: (code**)(), the parameters stayed the same
    
    Change-Id: I608be406969d7fa58f15ef36340aff4ad1ce9fd0
    Co-authored-by: Tim Zimmermann <tim@linux4.de>
    Signed-off-by: chaptsand <chaptsand@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -67,6 +67,10 @@ function blob_fixup() {
         vendor/lib64/hw/gatekeeper.mdfpp.so)
             ${PATCHELF} --replace-needed libcrypto.so libcrypto-v33.so "${2}"
             ;;
+        vendor/lib64/libsec-ril.so)
+            xxd -p -c0 "${2}" | sed "s/600e40f9e10315aa820c8052e30314aa/600e40f9e10315aa820c8052030080d2/g" | xxd -r -p > "${2}".patched
+            mv "${2}".patched "${2}"
+            ;;
         vendor/etc/vintf/manifest/sec_c2_manifest_default0_1_0.xml)
             sed -i 's/default0/software/g' "${2}"
             ;;
