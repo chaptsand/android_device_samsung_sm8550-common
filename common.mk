3b377ac48d766855895ea49936b27ee26c6414c7
chaptsand <chaptsand@gmail.com>
2024-03-31 17:10:28 +0800

    sm8550-common: audio-impl: Rebrand to samsung sm8550
    Change-Id: I60ddb98bc2cedc642ec9773be8cf1ee1891cf33b

--- common.mk
+++ common.mk
@@ -20,7 +20,7 @@ SOONG_CONFIG_android_hardware_audio += \
 SOONG_CONFIG_android_hardware_audio_run_64bit := true
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.1-impl \
+    android.hardware.audio@7.1-impl.samsung-sm8550 \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
