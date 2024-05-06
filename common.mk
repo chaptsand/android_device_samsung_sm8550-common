5e8e362687198d45f6a541fd0979afde0ecfce8c
Luka Panio <lukapanio@gmail.com>
2024-05-06 22:51:12 +0200

    sm8550-common: Build SamsungDoze
    Change-Id: I9a758f5617a71c9ab0f2680370e3e40710a6e0ae

--- common.mk
+++ common.mk
@@ -155,6 +155,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Doze
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
