58552103e4bf830fa57ea0db251d3dc21262798b
Luka Panio <lukapanio@gmail.com>
2024-05-06 22:52:12 +0200

    sm8550-common: Use pre-built power hal
    Change-Id: Id86ec09b10e6448d0fcc8f1cd002f9243ac49840

--- common.mk
+++ common.mk
@@ -345,12 +345,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-V2-ndk_platform.vendor \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.3.vendor
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/kalama/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Protobuf
 PRODUCT_PACKAGES += \
