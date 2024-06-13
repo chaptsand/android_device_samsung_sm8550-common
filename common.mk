50ecd21c296a4fff2e54b9db6b62cf670df8fa7b
Chirayu Desai <chirayudesai1@gmail.com>
2024-06-13 21:10:42 +0530

    sm8550-common: Don't set BOARD_API_LEVEL
    build/make/core/board_config.mk:1001: error: BOARD_API_LEVEL must not
    set manully. The build system automatically sets this value..
    
    Change-Id: If59ae9dea6cb13701d7217a1c0a355d4e07113eb

--- common.mk
+++ common.mk
@@ -409,9 +409,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
 # Shipping API
-BOARD_API_LEVEL := 33
-BOARD_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
-PRODUCT_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
+BOARD_SHIPPING_API_LEVEL := 33
+PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 
 # Telephony
 PRODUCT_PACKAGES += \
