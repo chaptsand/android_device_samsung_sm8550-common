a2339a2cc9b30d32aad0754acb70d42602e02225
Luka Panio <lukapanio@gmail.com>
2024-06-29 14:30:35 +0200

    sm8550-common: common: Move to samsung sensor service
    This fixes proximity sensor
    
    Change-Id: Idcfba31d8762cb443fe998aefe48cafe9226d92e

--- common.mk
+++ common.mk
@@ -408,7 +408,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors-service.multihal \
+    android.hardware.sensors-service.samsung-multihal \
     libsensorndkbridge \
     sensors.dynamic_sensor_hal
 
