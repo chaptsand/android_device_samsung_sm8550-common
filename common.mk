e2f026952f6e138f1308477b92cc0ff04860653d
Arian <arian.kulmer@web.de>
2024-08-06 20:14:27 +0800

    sm8550-common: Use soong_config_set to enable 64bit audio HAL
    Change-Id: Id8cca7234ae62fba402ae4eaafa5cd5cc33ce564

--- common.mk
+++ common.mk
@@ -20,11 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Audio
-SOONG_CONFIG_NAMESPACES += android_hardware_audio
-SOONG_CONFIG_android_hardware_audio += \
-    run_64bit
-SOONG_CONFIG_android_hardware_audio_run_64bit := true
-
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
