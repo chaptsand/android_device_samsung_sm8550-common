ded58571de5f1c6ce1187a98c07774ea21d2e71e
Bruno Martins <bgcngm@gmail.com>
2024-08-22 00:33:31 +0100

    sm8550-common: Drop 32-bit audio HAL and its dependencies
    Change-Id: I8edf07b896f286c9e55eb1cbf963d02bd8be6ab0

--- common.mk
+++ common.mk
@@ -34,6 +34,7 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
+    libaudioroute.vendor \
     libbatterylistener \
     libqcompostprocbundle \
     libqcomvisualizer \
