22efc463ed8a47051853527f594b6369e4b35e05
Bruno Martins <bgcngm@gmail.com>
2024-09-06 17:06:08 +0800

    sm8550-common: Build QTI health service
    Change-Id: I929a51b8516e5b3265f08160d0055f7dae6ab2bf

--- common.mk
+++ common.mk
@@ -97,6 +97,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery \
     android.hardware.health@1.0.vendor \
     android.hardware.health@2.1.vendor
 
