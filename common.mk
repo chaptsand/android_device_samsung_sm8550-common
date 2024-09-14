d019536694c2ff3ae29d0a7663429e86e7e31ae9
chaptsand <chaptsand@gmail.com>
2024-09-14 14:38:14 +0800

    sm8550-common: Switch to QTI common USB component
    Change-Id: I43091aaa20a2ebe5004b9228479971be9a9c6209

--- common.mk
+++ common.mk
@@ -224,7 +224,8 @@ TARGET_COMMON_QTI_COMPONENTS := \
     init \
     overlay \
     perf \
-    telephony
+    telephony \
+    usb
 
 # Sensors
 PRODUCT_PACKAGES += \
@@ -266,16 +267,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.systemhelper@1.0.vendor
 
 # USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
-
-PRODUCT_PACKAGES += \
-    init.samsung.usb.rc \
-    init.samsung.usb.sh
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+PRODUCT_HAS_GADGET_HAL := true
 
 # Verified Boot
 PRODUCT_COPY_FILES += \
