b078dc108463372a19cb47c5c6e10cf46ac89ce2
chaptsand <chaptsand@gmail.com>
2025-02-14 17:16:24 +0800

    sm8550-common: Switch to QTI common overlay component
    Change-Id: Ibf4abb00108601bf374a72eca359967b7ed9c5f6

--- common.mk
+++ common.mk
@@ -215,18 +215,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
-    CarrierConfigResCommon \
-    FrameworksResCommon \
-    FrameworksResSamsung \
-    FrameworksResTarget \
-    SettingsResCommon \
-    SettingsResSamsung \
-    SystemUIResCommon \
-    TelecommResCommon \
-    TelephonyResCommon \
-    WifiResCommon \
-    WifiResTarget \
-    WifiResTarget_spf
+    DmXqCarrierConfigRes \
+    DmXqFrameworksRes \
+    DmXqSecureElement \
+    DmXqSettingsRes
 
 # Partitions
 PRODUCT_PACKAGES += \
@@ -241,6 +233,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 TARGET_BOARD_PLATFORM := kalama
 TARGET_COMMON_QTI_COMPONENTS := \
     display \
+    overlay \
     perf
 
 # RIL
