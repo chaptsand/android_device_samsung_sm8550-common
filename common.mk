1d0adb9abcff7751825bea421769affba900cfcf
chaptsand <chaptsand@gmail.com>
2025-03-28 13:44:07 +0800

    sm8550-common: Move to Samsung health AIDL
    Change-Id: I7ebd8fe18fdb288042ad87c29e48cbe6ce4e989c

--- common.mk
+++ common.mk
@@ -137,8 +137,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti \
-    android.hardware.health-service.qti_recovery
+    android.hardware.health-service.samsung \
+    android.hardware.health-service.samsung-recovery
 
 # Hotword enrollment
 PRODUCT_COPY_FILES += \
