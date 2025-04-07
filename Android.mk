2bf80839d21119fea711c724e0c21550eda7e18b
chaptsand <chaptsand@gmail.com>
2025-04-07 22:51:03 +0800

    sm8550-common: Being Paranoid
    Change-Id: I72156e9991e222da6a808a509b6689c9fb010977

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter dm1q dm2q dm3q,$(TARGET_DEVICE)),)
+
+# A/B builds require us to create the mount points at compile time.
+# Just creating it for all cases since it does not hurt.
+FIRMWARE_MODEM_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware-modem
+$(FIRMWARE_MODEM_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $(FIRMWARE_MODEM_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/firmware-modem
+
+VM_SYSTEM_MOUNT_POINT := $(TARGET_OUT_VENDOR)/vm-system
+$(VM_SYSTEM_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $(VM_SYSTEM_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/vm-system
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+    $(FIRMWARE_MODEM_MOUNT_POINT) \
+    $(VM_SYSTEM_MOUNT_POINT)
+
+endif
