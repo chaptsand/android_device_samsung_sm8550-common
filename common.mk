946df2bdf568dd03732eb2491c9207b4d212b90f
chaptsand <chaptsand@gmail.com>
2025-04-25 21:51:34 +0800

    sm8550-common: Ship android.hardware.telephony.ims.xml
    Fix com.android.phone crash on startup after Android 15 QPR2 by enabling IMS
    feature, and ensure android.hardware.telephony.gsm.xml is not renamed.
    
    Change-Id: Ie8b700f138e67be70bae4ac5e2121615ad014dfc

--- common.mk
+++ common.mk
@@ -305,7 +305,8 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.samsung.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.hardware.telephony.mbms.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.mbms.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
