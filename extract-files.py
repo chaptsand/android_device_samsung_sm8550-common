3cf56187498fde537ced6e10f30e4f8dfb0c5cfe
Michael Bestas <mkbestas@lineageos.org>
2025-07-07 12:34:46 +0530

    sm8550-common: Copy media_codecs_vendor to media_codecs
    These are the right codecs to use for this device. The vendor codecs
    file isn't picked up without qcom framework changes.
    
    Change-Id: Ieecd2bfc14db2b396ca6257fdb2cd9361d9d7738
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -67,7 +67,7 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('default0', 'software'),
     'vendor/etc/init/vendor.qti.media.c2audio@1.0-service.rc': blob_fixup()
         .regex_replace('.*disabled.*\n', ''),
-    ('vendor/etc/media_codecs_kalama.xml', 'vendor/etc/media_codecs_kalama_vendor.xml'): blob_fixup()
+    ('vendor/etc/media_codecs_kalama.xml'): blob_fixup()
         .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('libcodec2_shim.so'),
