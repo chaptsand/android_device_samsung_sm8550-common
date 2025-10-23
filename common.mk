43521b8c7d3bfae3a561b1de0704e8465d74a47e
LuK1337 <priv.luk@gmail.com>
2025-10-23 23:18:07 +0200

    sm8550-common: Set PRODUCT_ENABLE_UFFD_GC to true
    This is not really necessary, but setting it prevents the build system
    from attempting to determine UFFD GC support on its own, during which
    it's triggering unnecessary kernel builds when compiling any app/jar.
    
    Change-Id: I8b8958f31c5437310ea27250447c8139e1684dcc

--- common.mk
+++ common.mk
@@ -191,6 +191,9 @@ PRODUCT_PACKAGES += \
     init.samsung.power.rc \
     init.samsung.rc
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+
 # Keymint
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore.xml
