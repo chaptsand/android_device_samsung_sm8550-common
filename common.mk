aospa-opace3/device_oneplus_sm8550-common

sm8550-common: Stop including kernel and modules namespaces
This looks like a bringup leftover and is not necessary.

Change-Id: If298a691a8ca114d75016c3f04ca50547f9786d0

common.mk
@@ -318,9 +318,7 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/oplus \
-    kernel/oneplus/sm8550 \
-    kernel/oneplus/sm8550-modules
+    hardware/oplus
 
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)

chaptsand/android_device_samsung_sm8550-common

common.mk
@@ -237,9 +237,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
-    hardware/samsung \
-    kernel/samsung/sm8550 \
-    kernel/samsung/sm8550-modules
+    hardware/samsung
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *